77b3af3762d14da057990ef6d5e83d272cd6d373
Ben Asselstine <bing@nym.hush.com>
2017-02-15 10:37:52 -0500

    added hillyer-square for making semi-magic squares

---
+++ src/hillyer-square.c
@@ -0,0 +1,202 @@
+/* Copyright (C) 2017 Ben Asselstine
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+#include <stdio.h>
+#include <argp.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include "magicsquareutil.h"
+
+//from:
+//http://www.multimagie.com/English/Morgenstern06.htm
+int in_binary;
+void (*display_square) (mpz_t s[3][3], FILE *out) = display_square_record;
+
+static void
+generate_hillyer_square (mpz_t sq[3][3], mpz_t k, mpz_t l)
+{
+  mpz_t k2, kl, l2, twokl, p, q, r, s, v, u;
+  mpz_t u2, p2, q2, r2, s2, v2, pq, twopq, rs, twors, uv, twouv;
+  mpz_inits (k2, kl, l2, twokl, p, q, r, s, v, u, NULL);
+  mpz_inits (u2, p2, q2, r2, s2, v2, pq, twopq, rs, twors, uv, twouv, NULL);
+
+  mpz_mul (k2, k, k);
+  mpz_mul (l2, l, l);
+  mpz_mul (kl, k, l);
+  mpz_mul_ui (twokl, kl, 2);
+
+  mpz_add (p, k2, kl);
+  mpz_add (p, p, l2);
+
+  mpz_sub (q, k2, l2);
+
+  mpz_add (r, k2, kl);
+  mpz_add (r, r, l2);
+
+  mpz_add (s, twokl, l2);
+
+  mpz_add (v, k2, kl);
+  mpz_add (v, v, l2);
+
+  mpz_add (u, twokl, k2);
+
+  mpz_mul (p2, p, p);
+  mpz_mul (q2, q, q);
+  mpz_mul (r2, r, r);
+  mpz_mul (s2, s, s);
+  mpz_mul (u2, u, u);
+  mpz_mul (v2, v, v);
+
+  mpz_mul (pq, p, q);
+  mpz_mul_ui (twopq, pq, 2);
+  mpz_mul (rs, r, s);
+  mpz_mul_ui (twors, rs, 2);
+  mpz_mul (uv, u, v);
+  mpz_mul_ui (twouv, uv, 2);
+
+  mpz_add (sq[0][0], u2, v2);
+  mpz_sub (sq[0][1], p2, q2);
+  mpz_sub (sq[0][1], sq[0][1], twopq);
+  mpz_abs (sq[0][1], sq[0][1]);
+
+  mpz_sub (sq[0][2], r2, s2);
+  mpz_add (sq[0][2], sq[0][2], twors);
+
+  mpz_sub (sq[1][0], p2, q2);
+  mpz_add (sq[1][0], sq[1][0], twopq);
+
+  mpz_add (sq[1][1], r2, s2);
+
+  mpz_sub (sq[1][2], u2, v2);
+  mpz_sub (sq[1][2], sq[1][2], twouv);
+  mpz_abs (sq[1][2], sq[1][2]);
+
+  mpz_sub (sq[2][0], r2, s2);
+  mpz_sub (sq[2][0], sq[2][0], twors);
+  mpz_abs (sq[2][0], sq[2][0]);
+
+  mpz_sub (sq[2][1], u2, v2);
+  mpz_add (sq[2][1], sq[2][1], twouv);
+
+  mpz_add (sq[2][2], p2, q2);
+
+  for (int i = 0; i < 3; i++)
+    for (int j = 0; j < 3; j++)
+      mpz_mul (sq[i][j], sq[i][j], sq[i][j]);
+  mpz_clears (u2, p2, q2, r2, s2, v2, pq, twopq, rs, twors, uv, twouv, NULL);
+  mpz_clears (k2, kl, l2, twokl, p, q, r, s, v, u, NULL);
+}
+
+int
+hillyer (FILE *in, FILE *out)
+{
+  ssize_t read;
+  char *line = NULL;
+  size_t len = 0;
+  mpz_t k, l, sq[3][3];
+  mpz_inits (k, l, NULL);
+  for (int i = 0; i < 3; i++)
+    for (int j = 0; j < 3; j++)
+      mpz_init (sq[i][j]);
+  while (1)
+    {
+      read = getdelim (&line, &len, ',', in);
+      if (read == -1)
+        break;
+      char *comma = strchr (line, ',');
+      if (comma)
+        *comma = '\0';
+      mpz_set_str (k, line, 10);
+      read = getline (&line, &len, in);
+      if (read == -1)
+        break;
+      mpz_set_str (l, line, 10);
+      generate_hillyer_square (sq, k, l);
+      display_square (sq, out);
+    }
+  for (int i = 0; i < 3; i++)
+    for (int j = 0; j < 3; j++)
+      mpz_clear (sq[i][j]);
+  mpz_clears (k, l, NULL);
+  if (line)
+    free (line);
+  return 0;
+}
+
+static int
+binary_hillyer (FILE *in, FILE *out)
+{
+  ssize_t read;
+  mpz_t k, l, sq[3][3];
+  mpz_inits (k, l, NULL);
+  for (int i = 0; i < 3; i++)
+    for (int j = 0; j < 3; j++)
+      mpz_init (sq[i][j]);
+  while (1)
+    {
+      read = mpz_inp_raw (k, in);
+      if (!read)
+        break;
+      read = mpz_inp_raw (l, in);
+      if (!read)
+        break;
+      generate_hillyer_square (sq, k, l);
+      display_square (sq, out);
+    }
+  for (int i = 0; i < 3; i++)
+    for (int j = 0; j < 3; j++)
+      mpz_clear (sq[i][j]);
+  mpz_clears (k, l, NULL);
+  return 0;
+}
+
+static error_t
+parse_opt (int key, char *arg, struct argp_state *state)
+{
+  switch (key)
+    {
+    case 'i':
+      in_binary = 1;
+      break;
+    case 'o':
+      display_square = display_binary_square_record;
+      break;
+    }
+  return 0;
+}
+
+static struct argp_option
+options[] =
+{
+  { "in-binary", 'i', 0, 0, "Input raw GMP numbers instead of text"},
+  { "out-binary", 'o', 0, 0, "Output raw GMP numbers instead of text"},
+  { 0 }
+};
+
+struct argp argp ={options, parse_opt, 0, "Generate a nearly-magic 3x3 square according to Lee Morgenstern's algorithm that employes hillyer's formula on multimagie.com.  These squares are generated from two input values on the standard input, K and L, separated by a comma." , 0};
+
+
+int
+main (int argc, char **argv)
+{
+  setenv ("ARGP_HELP_FMT", "no-dup-args-note", 1);
+  argp_parse (&argp, argc, argv, 0, 0, 0);
+  int ret;
+  if (in_binary)
+    ret = binary_hillyer (stdout, stdout);
+  else
+    ret = hillyer (stdin, stdout);
+  return ret;
+}
